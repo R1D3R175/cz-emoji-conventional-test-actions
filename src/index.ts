b964b3925cebc4b1732a45e9bbbca2a2f6025ea7
R1D3R175 <r1.d3.r175@gmail.com>
2025-03-20 05:57:20 +0100

    ✨ feat(epic comment): it's working and i don't know why

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import wrap from "word-wrap";
 import { gitmojiByType, types } from "./constant";
 
 // this is a comment because I want to test release drafter config.
+// another comment because testtttting
 const config = configLoader.load() || {};
 
 const customTypesPath = path.resolve(__dirname, "../../../czCustomTypes.cjs");
